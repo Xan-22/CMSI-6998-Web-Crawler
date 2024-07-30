3cf425eb24405de1a26223bc033931c10275205b
Xan-22 <83319450+Xan-22@users.noreply.github.com>
2024-07-30 15:13:13 -0700

    added site name to document

--- project/scraper.py
+++ project/scraper.py
@@ -16,9 +16,9 @@ CHROME_OPTIONS.add_argument("--no-sandbox")  # Bypass OS security model, REQUIRE
 CHROME_OPTIONS.add_argument("--disable-dev-shm-usage")  # Overcome limited resource problems
 
 class WebCrawler:
-    def __init__(self, url_base, iden):
+    def __init__(self, url_base, site_name):
 
-        self.iden = iden # Identifier for the crawler used in output and Redis keys
+        self.site_name = site_name
         print(f"{self.iden}: Initializing WebCrawler for domain: {url_base}")
         self.url_base = url_base
         self.page_num = 1
@@ -27,30 +27,35 @@ class WebCrawler:
         ###### Set up the Selenium webdrivers ######
         self.base_wd = webdriver.Chrome(options=CHROME_OPTIONS)
         self.base_wd.get(self.url_base)
-        print(f"{self.iden}: Initialized Base WebDriver. Waiting 2 seconds...")
-        time.sleep(2)
+        print(f"{self.iden}: Initialized Base WebDriver")
+        time.sleep(2) # Allow time for the web page to open
         self.article_wd = webdriver.Chrome(options=CHROME_OPTIONS)
         self.article_wd.get(self.url_base)
         print(f"{self.iden}: Initialized Article WebDriver")
-        time.sleep(5) # Allow time for the web pages to open
+        time.sleep(2) # Allow time for the web page to open
 
         self.webdriver_scroll_pause_time = 1
         self.webdriver_screen_height = self.base_wd.execute_script("return window.screen.height;")  # Get the screen height
 
 
-        ###### Initialize Elasticsearch ######
-        username = 'elastic'
-        password = os.getenv('ELASTIC_PASSWORD') # should be 'elastic' for now. make sure it's in the terminal environment.
+        ###### Initialize Elasticsearch client ######
+        es_username = 'elastic'
+        es_password = os.getenv('ELASTIC_PASSWORD')
+        es_cloud_id = os.getenv('ELASTIC_CLOUD_ID')
+
         self.es_client = Elasticsearch(
-           "http://localhost:9200",
-           basic_auth=(username, password)
+            cloud_id=es_cloud_id,
+            http_auth=(es_username, es_password)
+           #"http://localhost:9200", # For local testing
+           #basic_auth=(es_username, es_password)
         )
         print(f"{self.iden}: {self.es_client.info()}") # Test
 
-        ###### Initialize Redis ######
+
+        ###### Initialize Redis client ######
         print(f"{self.iden}: Initializing Redis cache")
         self.r = redis.Redis()
-        self.r.flushall()
+        self.r.delete(f"{self.iden}-links")
 
         # Run the crawler on initialization
         print(f"{self.iden}: Initialization complete. Running crawler...")
@@ -69,7 +74,7 @@ class WebCrawler:
         self.article_wd.quit()
 
 
-    def scroll_page(self, webdriver, num_pages_to_scroll=5):
+    def scroll_page(self, webdriver, num_pages_to_scroll=1):
         # Scroll down num_pages screens(pages) max of html content each time this method is called
         print(f"{self.iden}: Scrolling {webdriver.current_url}")
         stopping_point = self.page_num + num_pages_to_scroll
@@ -86,12 +91,12 @@ class WebCrawler:
                 break
 
 
-    def write_to_elastic_webpages(self, decoded_url, html):
-        self.es_client.index(index='webpages', document={ 'url': decoded_url, 'html': html })
+    def write_to_elastic_webpages(self, decoded_url, domain):
+        self.es_client.index(index='webpages', document={ 'url': decoded_url, 'domain': domain })
 
 
-    def write_to_elastic_articles(self, headline, date, author, body, topics):
-        self.es_client.index(index='articles', document={ 'headline': headline, 'date': date, 'author': author, 'body': body, 'topics': topics })
+    def write_to_elastic_articles(self, site, headline, date, author, body, topics):
+        self.es_client.index(index='articles', document={ 'site': site, 'headline': headline, 'date': date, 'author': author, 'body': body, 'topics': topics })
 
 
     def start_crawl(self, url):
@@ -102,19 +107,19 @@ class WebCrawler:
         # Scrape each article
         while link := self.r.rpop(f"{self.iden}-links"):
             self.scrape_data(self.url_base, link)
-            print(f"{self.iden}: Scraped. Waiting 5 seconds...")
-            time.sleep(5) # Be REALLY nice to the server
+            print(f"{self.iden}: Scraped. Waiting a few seconds...")
+            time.sleep(3) # Be REALLY nice to the server
 
 
     def scrape_links(self, url_base, webdriver):
         print(f"{self.iden}: Scraping Links from: {url_base}")
-        self.scroll_page(webdriver, 5)
+        self.scroll_page(webdriver, 2)
 
         attempts = 0
         links = []
-        while (len(links) == 0 and attempts < 10):
+        while (len(links) == 0 and attempts < 5):
             attempts += 1
-            time.sleep(5) # Wait for the page to load
+            time.sleep(2) # Wait for the page to load
             soup = BeautifulSoup(webdriver.page_source, "html.parser") # This can probably be done without creating the object multiple times
             a_tags = soup.find_all("a")
             hrefs = [ a.get("href") for a in a_tags ]
@@ -123,19 +128,17 @@ class WebCrawler:
             match (url_base):
                 case "https://www.pcgamer.com/": # PCGamer puts the whole URL in their hrefs
                     print(f"{self.iden}: PCGamer URL filtering")
-                    filtered = [ a for a in hrefs if a and self.check_filters(url_base, a) ]
+                    filtered = [ a for a in hrefs if a and self.check_filters(url_base, a) and self.es_client.exists(index='webpages', id=a) == False ]
                 case default:
                     print(f"{self.iden}: Generic URL filtering")
-                    filtered = [ self.url_base + a for a in hrefs if a and self.check_filters(url_base, a) ]
+                    filtered = [ self.url_base + a for a in hrefs if a and self.check_filters(url_base, a) and self.es_client.exists(index='webpages', id=(self.url_base + a)) == False ]
             links = list(set(filtered)) # Remove duplicates
-
-            # Put urls in Redis queue
-            # create a linked list in Redis, call it "(name)-links"
             print(f"{self.iden}: Found {len(links)} links")
         if (len(links) > 0):
+            # Put links into a queue in Redis, call it "(name)-links"
             self.r.lpush(f"{self.iden}-links", *links)
         else:
-            print(f"{self.iden}: No links found after 10 attempts. Exiting.")
+            print(f"{self.iden}: No links found after 5 attempts. Exiting.")
             self.has_links = False
             return
 
@@ -167,7 +170,7 @@ class WebCrawler:
                         body_text += p.get_text() + "\n"
                     topics = [ t.get_text() for t in soup.find_all("a", attrs={"data-cy": "object-breadcrumb"}) ]
                 case "https://www.gameinformer.com/":
-                    headline = "GameInformer: " + soup.find("h1", class_="page-title").get_text()
+                    headline = soup.find("h1", class_="page-title").get_text()
                     authors = [ a.get_text() for a in soup.find("div", class_="author-details").find_all("a") ]
                     date = datetime.strptime(soup.find("div", class_="author-details").get_text().split("on ")[1], "%b %d, %Y at %I:%M %p").strftime("%Y-%m-%d")
                     body = soup.find("div", class_="ds-main").find_all("p")
@@ -176,7 +179,7 @@ class WebCrawler:
                         body_text += p.get_text() + "\n"
                     topics = [ t.get_text().strip("\n") for t in soup.find("div", class_="gi5--product--summary").find_all("a", attrs={"rel": "bookmark"}) ]
                 case "https://www.pcgamer.com/":
-                    headline = "PCGamer: " + soup.find("h1").get_text()
+                    headline = soup.find("h1").get_text()
                     authors = [ a.get_text() for a in soup.find("div", class_="author-byline__authors").find_all("a", class_="link author-byline__link") ]
                     date = datetime.strptime(soup.find("meta", attrs={"name": "pub_date"}).get("content").split("T")[0], "%Y-%m-%d").strftime("%Y-%m-%d")
                     body = soup.find("div", attrs={"id" : "article-body"}).find_all("p", class_=None)
@@ -187,7 +190,7 @@ class WebCrawler:
         except Exception as e:
             print(f"{self.iden}: Invalid article format. Error: {e}\nSkipping...")
         else:
-            self.write_to_elastic_articles(headline, date, authors, body_text, topics)
+            self.write_to_elastic_articles(self.site_name, headline, date, authors, body_text, topics)
 
 
     def check_filters(self, url_base, href):
